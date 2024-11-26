b1e5d59b6ab64f65c174475b8889212a36ef97d2
Bread Grocery <breadgrocery@gmail.com>
2024-11-26 11:13:00 +0800

    fix: subsequent runs copied files to the wrong place

--- install.ps1
+++ install.ps1
@@ -9,21 +9,22 @@ Write-Output "Install Windows Terminal..."
 $installation = Read-Host "Please enter installation path (default: C:\Program Files\Windows Terminal)"
 if ($installation -eq "") {
     $installation = "C:\Program Files\Windows Terminal"
-}else{
-    $installation = "$installation\Windows Terminal"
 }
-Copy-Item -Path "apps\Windows Terminal" -Destination $installation -Recurse -Force
+$windows_terminal_base = "$installation\Windows Terminal"
+New-Item -Force -Type Directory $windows_terminal_base
+Copy-Item -Path "apps\Windows Terminal\*" -Destination "$windows_terminal_base" -Recurse -Force
 Copy-Item -Path "settings.json" -Destination "$env:LOCALAPPDATA\Microsoft\Windows Terminal" -Force
 
 Write-Output "Add Windows Terminal to context menu..."
 reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\runas" /v ShowBasedOnVelocityId /t REG_DWORD /d 0x639bc8 /f
-reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\runas" /v icon /t REG_SZ /d "$installation\Windows Terminal\WindowsTerminal.exe" /f
+reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\runas" /v icon /t REG_SZ /d "$windows_terminal_base\WindowsTerminal.exe" /f
 reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\runas" /t REG_SZ /d "Open Windows Terminal" /f
-reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\runas\command" /t REG_SZ /d "$installation\Windows Terminal\WindowsTerminal.exe" /f
+reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\runas\command" /t REG_SZ /d "$windows_terminal_base\WindowsTerminal.exe" /f
 
 Write-Output "Install Clink..."
 $clink_base = "$installation\clink"
-Copy-Item -Path "apps\clink" -Destination $clink_base -Recurse -Force
+New-Item -Force -Type Directory $clink_base
+Copy-Item -Path "apps\clink\*" -Destination $clink_base -Recurse -Force
 Copy-Item -Path "integrations\cmd\oh-my-posh.lua" -Destination $clink_base -Force
 [Environment]::SetEnvironmentVariable("CLINK_HOME", "$clink_base", "Machine")
 
@@ -51,7 +52,8 @@ foreach ($font in $fonts) {
 
 Write-Output "Install oh-my-posh..."
 $oh_my_posh_home = "$installation\oh-my-posh"
-Copy-Item -Path "apps\oh-my-posh" -Destination $oh_my_posh_home -Recurse -Force
+New-Item -Force -Type Directory $oh_my_posh_home
+Copy-Item -Path "apps\oh-my-posh\*" -Destination $oh_my_posh_home -Recurse -Force
 [Environment]::SetEnvironmentVariable("Path", "$env:Path;$oh_my_posh_home\bin", "Machine")
 [Environment]::SetEnvironmentVariable("POSH_INSTALLER", "manual", "Machine")
 [Environment]::SetEnvironmentVariable("POSH_THEMES_PATH", "$oh_my_posh_home\themes", "Machine")
